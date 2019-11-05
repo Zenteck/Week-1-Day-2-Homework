c5fb9ca18fe885e7b23d22f065567f8fc74ab714
Alan <spambox+github@zenteck.co.uk>
2019-11-05 16:47:04 +0000

    MVP

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,27 @@ end
 def add_string_as_number(string_num_1, string_num_2)
   return string_num_1.to_i + string_num_2.to_i
 end
+
+def number_to_full_month_name(number)
+  case number
+  when 1
+    return "January"
+  when 3
+    return "March"
+  when 9
+    return "September"
+end
+#
+end
+
+def number_to_short_month_name(month)
+  case month
+  when 1
+    return "Jan"
+  when 4
+    return "Apr"
+  when 10
+    return "Oct"
+  end
+  #
+end
