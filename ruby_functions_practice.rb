07f7c4e010addca2cd9b0277f5c30649636c86e3
Alan <spambox+github@zenteck.co.uk>
2019-11-05 16:27:28 +0000

    third

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -2,4 +2,30 @@ def return_10
   return 10
 end
 
-return_10
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string, string_2)
+  return "#{string}#{string_2}"
+end
+
+def add_string_as_number(string_num_1, string_num_2)
+  return string_num_1.to_i + string_num_2.to_i
+end
